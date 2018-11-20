a4f6b729e892517f129d20e0f34c7cfef239c2a4
Mark Bates <mark@markbates.com>
2018-11-20 14:31:07 -0500

    validate maps

--- genny/maps/map.go
+++ genny/maps/map.go
@@ -1,15 +1,50 @@
 package maps
 
-import "html/template"
+import (
+	"errors"
+	"html/template"
+)
 
 // Map represents an implementation of the map
 type Map struct {
-	Name    template.HTML
-	GoType  template.HTML
-	Zero    template.HTML
-	Package string
-	A       template.HTML
-	B       template.HTML
-	BB      template.HTML
-	C       template.HTML
+	Destination string
+	Package     string
+	Name        template.HTML
+	GoType      template.HTML
+	Zero        template.HTML
+	A           template.HTML
+	B           template.HTML
+	BB          template.HTML
+	C           template.HTML
+}
+
+func (m Map) Validate() error {
+	if m.Destination == "" {
+		return errors.New("you must set a destination package")
+	}
+
+	if m.GoType == "" {
+		return errors.New("you must set a go type")
+	}
+
+	if m.Zero == "" {
+		return errors.New("you must set a zero value")
+	}
+
+	if m.A == "" {
+		return errors.New("you must set a A value")
+	}
+
+	if m.B == "" {
+		return errors.New("you must set a B value")
+	}
+
+	if m.BB == "" {
+		return errors.New("you must set a BB value")
+	}
+
+	if m.C == "" {
+		return errors.New("you must set a C value")
+	}
+	return nil
 }
--- genny/maps/options.go
+++ genny/maps/options.go
@@ -1,5 +1,7 @@
 package maps
 
+import "errors"
+
 // Options for a sync.Map implementation
 type Options struct {
 	Maps []Map
@@ -7,5 +9,14 @@ type Options struct {
 
 // Validate that options are usuable
 func (opts *Options) Validate() error {
+	if len(opts.Maps) == 0 {
+		return errors.New("you must supply at least one map")
+	}
+
+	for _, m := range opts.Maps {
+		if err := m.Validate(); err != nil {
+			return err
+		}
+	}
 	return nil
 }
